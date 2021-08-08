9e77dc0030b49cb6524c659c51b3edda0cc8295a
Gustavo Gallegos <meaning.app>
2021-08-08 14:31:15 -0700

    Fix ring focus on settings icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,7 +12,7 @@ const Header = (): JSX.Element => {
           Bitcoin Expense Calculator
         </div>
         <button
-          className="inline-flex justify-center items-center h-7 sm:h-12 rounded-full border border-transparent px-0 py-0 text-base font-medium text-white   focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-100 sm:text-sm"
+          className="inline-flex justify-center items-center h-7 sm:h-12 rounded-full border border-transparent px-0 py-0 text-base font-medium text-white   focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
           onClick={() => toggle()}
         >
           <SettingsIcon className="h-6 w-6 sm:h-8 sm:w-8  text-gray-500 hover:text-indigo-700" />
